4e56884b053f5c9f89141b659dff8e26931b7765
Jeff Barczewski <jeff.barczewski@gmail.com>
2018-09-07 16:40:42 -0500

    remove createCurriedAction

--- src/createCurriedAction.js
+++
@@ -1,5 +0,0 @@
-import curry from 'lodash.curry';
-import createAction from './createAction';
-
-export default (type, payloadCreator) =>
-  curry(createAction(type, payloadCreator), payloadCreator.length);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import combineActions from './combineActions';
 import createAction from './createAction';
 import createActions from './createActions';
-import createCurriedAction from './createCurriedAction';
 import createReducer from './createReducer';
 
-export {
-  combineActions,
-  createAction,
-  createActions,
-  createCurriedAction,
-  createReducer
-};
+export { combineActions, createAction, createActions, createReducer };
